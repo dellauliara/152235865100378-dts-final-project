9c80133028de649be252c7a3cbc1457af3191ec1
dela <diella.aulia@gmail.com>
2022-08-02 09:59:51 +0700

    part5a

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_KEY = 'b3c92fe53af44f178e647febf833de93';
+//const API_KEY = 'b3c92fe53af44f178e647febf833de93';
 //const API_KEY = 'e100c22111ba472fa40d0f41c0c4cc05';
 //const API_KEY = '9b2bac2a67fd478a8d917623b9c3b386';
-//const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
 
 const baseUrl = 'https://api.spoonacular.com/recipes/';
 
